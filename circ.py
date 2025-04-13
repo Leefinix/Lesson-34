5e158ef72c0bbe44a7a2a88464ef63dc6e9e961b
Leefinix <joaquinnleo@gmail.com>
2025-04-13 19:14:23 +0700

    co

--- circ.py
+++ circ.py
@@ -4,7 +4,7 @@ pygame.init()
 window = pygame.display.set_mode((400, 400))
 window.fill((225, 255, 255))
 green = (0, 255, 0)
-pygame.draw.circle(window, green, (300, 300), 50)
+pygame.draw.circle(window, green, (100, 100), 50)
 pygame.draw.circle(window, green, (300, 300), 50, 3)
 pygame.display.update()
 running = True
